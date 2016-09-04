5c9b79f23e7f0a088c0ed09459f817b46f5b1fd6
manulaiko <manulaiko@gmail.com>
2016-09-04 07:54:48 +0200

    Created internal page controller

---
+++ alexya/Application/Page/Controller/Internal.php
@@ -0,0 +1,55 @@
+<?php
+namespace Application\Page\Controller;
+
+use \Alexya\Foundation\{
+    Controller,
+    Module
+};
+use \Alexya\Http\{
+    Response,
+    Request
+};
+
+use \Exception;
+
+/**
+ * Internal page controller.
+ *
+ * Loads the requested module.
+ *
+ * @author Manulaiko <manulaiko@gmail.com>
+ */
+class Internal extends Controller
+{
+    /**
+     * Loads and executes the requested module.
+     *
+     * @return string Response content
+     */
+    public function index() : string
+    {
+        $uri = $this->_triad->request->uri();
+
+        if(
+            count($uri) < 2 ||
+            empty($uri[1])
+        ) {
+            Response::redirect("/Internal/Start");
+        }
+
+        $body = "";
+        try {
+            $moduleURI = implode("/", array_slice($uri, 2));
+            $module    = new Module($uri[2], new Request($moduleURI));
+
+            $this->_triad->children->module = $module;
+
+            $body = $this->_triad->View->render();
+        } catch(Exception $e) {
+            throw $e;
+            //Response::redirect("/internal/Start");
+        }
+
+        return $body;
+    }
+}
