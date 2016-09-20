419afcf0c5714db1d2237c0de916d518cf3cfec3
Manulaiko <manulaiko@gmail.com>
2016-09-20 16:38:08 +0200

    Created ORM class for 'accounts_equipment_items' table

---
+++ alexya/Application/ORM/Account/Equipment/Item.php
@@ -0,0 +1,30 @@
+<?php
+namespace Application\ORM\Account\Equipment;
+
+use \Alexya\Database\ORM\Model;
+
+/**
+ * Model for `accounts_equipment_items` table.
+ *
+ * @author Manulaiko <manulaiko@gmail.com>
+ */
+class Item extends Model
+{
+    /**
+     * Table name.
+     *
+     * @var string
+     */
+    protected static $_table = "accounts_equipment_items";
+
+    /**
+     * Relations.
+     *
+     * @var array
+     */
+    protected static $_relations = [
+        "items" => [
+            "name" => "Item"
+        ]
+    ];
+}
