54e64f8de14f3ca360ecd51ad24d7c6e1c35393e
Manulaiko <manulaiko@gmail.com>
2017-03-29 13:36:09 +0200

    Imported spacemap

---
+++ www/spacemap/xml/maps.php
@@ -0,0 +1,269 @@
+<maps>
+	<map id="1">
+		<gameserverIP>127.0.0.1</gameserverIP>
+	</map>
+	<map id="2">
+    	<gameserverIP>127.0.0.1</gameserverIP>
+    </map>
+	<map id="3">
+		<gameserverIP>127.0.0.1</gameserverIP>
+	</map>
+	<map id="4">
+		<gameserverIP>127.0.0.1</gameserverIP>
+	</map>
+	<map id="5">
+		<gameserverIP>127.0.0.1</gameserverIP>
+	</map>
+	<map id="6">
+		<gameserverIP>127.0.0.1</gameserverIP>
+	</map>
+	<map id="7">
+		<gameserverIP>127.0.0.1</gameserverIP>
+	</map>
+	<map id="8">
+		<gameserverIP>127.0.0.1</gameserverIP>
+	</map>
+	<map id="9">
+		<gameserverIP>127.0.0.1</gameserverIP>
+	</map>
+	<map id="10">
+		<gameserverIP>127.0.0.1</gameserverIP>
+	</map>
+	<map id="11">
+		<gameserverIP>127.0.0.1</gameserverIP>
+	</map>
+	<map id="12">
+		<gameserverIP>127.0.0.1</gameserverIP>
+	</map>
+	<map id="13">
+		<gameserverIP>127.0.0.1</gameserverIP>
+	</map>
+	<map id="14">
+		<gameserverIP>127.0.0.1</gameserverIP>
+	</map>
+	<map id="15">
+		<gameserverIP>127.0.0.1</gameserverIP>
+	</map>
+	<map id="16">
+		<gameserverIP>127.0.0.1</gameserverIP>
+	</map>
+	<map id="17">
+		<gameserverIP>127.0.0.1</gameserverIP>
+	</map>
+	<map id="18">
+		<gameserverIP>127.0.0.1</gameserverIP>
+	</map>
+	<map id="19">
+		<gameserverIP>127.0.0.1</gameserverIP>
+	</map>
+	<map id="20">
+		<gameserverIP>127.0.0.1</gameserverIP>
+	</map>
+	<map id="21">
+		<gameserverIP>127.0.0.1</gameserverIP>
+	</map>
+	<map id="22">
+		<gameserverIP>127.0.0.1</gameserverIP>
+	</map>
+	<map id="23">
+		<gameserverIP>127.0.0.1</gameserverIP>
+	</map>
+	<map id="24">
+		<gameserverIP>127.0.0.1</gameserverIP>
+	</map>
+	<map id="25">
+		<gameserverIP>127.0.0.1</gameserverIP>
+	</map>
+	<map id="26">
+		<gameserverIP>127.0.0.1</gameserverIP>
+	</map>
+	<map id="27">
+		<gameserverIP>127.0.0.1</gameserverIP>
+	</map>
+	<map id="28">
+		<gameserverIP>127.0.0.1</gameserverIP>
+	</map>
+	<map id="29">
+		<gameserverIP>127.0.0.1</gameserverIP>
+	</map>
+	<map id="42">
+		<gameserverIP>127.0.0.1</gameserverIP>
+	</map>
+	<map id="51">
+		<gameserverIP>127.0.0.1</gameserverIP>
+	</map>
+	<map id="52">
+		<gameserverIP>127.0.0.1</gameserverIP>
+	</map>
+	<map id="53">
+		<gameserverIP>127.0.0.1</gameserverIP>
+	</map>
+	<map id="54">
+		<gameserverIP>127.0.0.1</gameserverIP>
+	</map>
+	<map id="55">
+		<gameserverIP>127.0.0.1</gameserverIP>
+	</map>
+	<map id="56">
+		<gameserverIP>127.0.0.1</gameserverIP>
+	</map>
+	<map id="57">
+		<gameserverIP>127.0.0.1</gameserverIP>
+	</map>
+	<map id="58">
+		<gameserverIP>127.0.0.1</gameserverIP>
+	</map>
+	<map id="61">
+		<gameserverIP>127.0.0.1</gameserverIP>
+	</map>
+	<map id="62">
+		<gameserverIP>127.0.0.1</gameserverIP>
+	</map>
+	<map id="63">
+		<gameserverIP>127.0.0.1</gameserverIP>
+	</map>
+	<map id="64">
+		<gameserverIP>127.0.0.1</gameserverIP>
+	</map>
+	<map id="65">
+		<gameserverIP>127.0.0.1</gameserverIP>
+	</map>
+	<map id="66">
+		<gameserverIP>127.0.0.1</gameserverIP>
+	</map>
+	<map id="67">
+		<gameserverIP>127.0.0.1</gameserverIP>
+	</map>
+	<map id="68">
+		<gameserverIP>127.0.0.1</gameserverIP>
+	</map>
+	<map id="69">
+		<gameserverIP>127.0.0.1</gameserverIP>
+	</map>
+	<map id="70">
+		<gameserverIP>127.0.0.1</gameserverIP>
+	</map>
+	<map id="71">
+		<gameserverIP>127.0.0.1</gameserverIP>
+	</map>
+	<map id="72">
+		<gameserverIP>127.0.0.1</gameserverIP>
+	</map>
+	<map id="73">
+		<gameserverIP>127.0.0.1</gameserverIP>
+	</map>
+	<map id="74">
+		<gameserverIP>127.0.0.1</gameserverIP>
+	</map>
+	<map id="75">
+		<gameserverIP>127.0.0.1</gameserverIP>
+	</map>
+	<map id="76">
+		<gameserverIP>127.0.0.1</gameserverIP>
+	</map>
+	<map id="77">
+		<gameserverIP>127.0.0.1</gameserverIP>
+	</map>
+	<map id="78">
+		<gameserverIP>127.0.0.1</gameserverIP>
+	</map>
+	<map id="81">
+		<gameserverIP>127.0.0.1</gameserverIP>
+	</map>
+	<map id="82">
+		<gameserverIP>127.0.0.1</gameserverIP>
+	</map>
+	<map id="91">
+		<gameserverIP>127.0.0.1</gameserverIP>
+	</map>
+	<map id="92">
+		<gameserverIP>127.0.0.1</gameserverIP>
+	</map>
+	<map id="93">
+		<gameserverIP>127.0.0.1</gameserverIP>
+	</map>
+	<map id="101">
+		<gameserverIP>127.0.0.1</gameserverIP>
+	</map>
+	<map id="102">
+		<gameserverIP>127.0.0.1</gameserverIP>
+	</map>
+	<map id="103">
+		<gameserverIP>127.0.0.1</gameserverIP>
+	</map>
+	<map id="104">
+		<gameserverIP>127.0.0.1</gameserverIP>
+	</map>
+	<map id="105">
+		<gameserverIP>127.0.0.1</gameserverIP>
+	</map>
+	<map id="106">
+		<gameserverIP>127.0.0.1</gameserverIP>
+	</map>
+	<map id="107">
+		<gameserverIP>127.0.0.1</gameserverIP>
+	</map>
+	<map id="108">
+		<gameserverIP>127.0.0.1</gameserverIP>
+	</map>
+	<map id="109">
+		<gameserverIP>127.0.0.1</gameserverIP>
+	</map>
+	<map id="110">
+		<gameserverIP>127.0.0.1</gameserverIP>
+	</map>
+	<map id="111">
+		<gameserverIP>127.0.0.1</gameserverIP>
+	</map>
+	<map id="112">
+		<gameserverIP>127.0.0.1</gameserverIP>
+	</map>
+	<map id="113">
+		<gameserverIP>127.0.0.1</gameserverIP>
+	</map>
+	<map id="114">
+		<gameserverIP>127.0.0.1</gameserverIP>
+	</map>
+	<map id="115">
+		<gameserverIP>127.0.0.1</gameserverIP>
+	</map>
+	<map id="116">
+		<gameserverIP>127.0.0.1</gameserverIP>
+	</map>
+	<map id="117">
+		<gameserverIP>127.0.0.1</gameserverIP>
+	</map>
+	<map id="118">
+		<gameserverIP>127.0.0.1</gameserverIP>
+	</map>
+	<map id="119">
+		<gameserverIP>127.0.0.1</gameserverIP>
+	</map>
+	<map id="120">
+		<gameserverIP>127.0.0.1</gameserverIP>
+	</map>
+	<map id="121">
+		<gameserverIP>127.0.0.1</gameserverIP>
+	</map>
+	<map id="200">
+		<gameserverIP>127.0.0.1</gameserverIP>
+	</map>
+	<map id="201">
+		<gameserverIP>127.0.0.1</gameserverIP>
+	</map>
+	<map id="202">
+		<gameserverIP>127.0.0.1</gameserverIP>
+	</map>
+	<map id="203">
+		<gameserverIP>127.0.0.1</gameserverIP>
+	</map>
+	<map id="222">
+		<gameserverIP>127.0.0.1</gameserverIP>
+	</map>
+	<map id="223">
+		<gameserverIP>127.0.0.1</gameserverIP>
+	</map>
+	<map id="224">
+		<gameserverIP>127.0.0.1</gameserverIP>
+	</map>
+</maps>
