343fb7eff9c048475b6a3b7dc9a19b8f2b6bfb80
Manulaiko <manulaiko@gmail.com>
2017-03-29 14:07:36 +0200

    Fix shit

--- alexya/Application/Page/Presenter/Internal.php
+++ alexya/Application/Page/Presenter/Internal.php
@@ -19,13 +19,6 @@ class Internal extends Presenter
     {
         $name = ($this->_request->uri()[2] ?? "");
 
-        if(
-            $name == "CompanyChoose" ||
-            $name == "Map"
-        ) {
-            return $this->_triad->children->module->Controller->render();
-        }
-
         $this->_triad->View->set("name", $name);
         $this->_setFramesFlag($name);
 
@@ -35,8 +28,17 @@ class Internal extends Presenter
             }
         }
 
+        $module = $this->_triad->children->module->Controller->render();
+
         // Call the render method here so it redirects now instead of when the view is being rendered.
-        $this->_triad->View->set("module", $this->_triad->children->module->Controller->render());
+        $this->_triad->View->set("module", $module);
+
+        if(
+            $name == "CompanyChoose" ||
+            $name == "Map"
+        ) {
+            return $module;
+        }
 
         // TODO $this->_triad->View->set("news", Model::latest(10, "id", "news"));
 
